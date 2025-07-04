3548d68c6d53c0df8d7d24de997ceaa08407d5e2
b-isry <bisratt1995@gmail.com>
2025-07-04 10:44:00 +0300

    refactor: refactor rebuild.go to rebuild the nodemodules from the s3 cache

--- cmd/rebuild.go
+++ cmd/rebuild.go
@@ -1,12 +1,16 @@
-// cmd/rebuild.go
 package cmd
 
 import (
+	"context"
 	"encoding/json"
 	"io"
 	"os"
 	"path/filepath"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -28,10 +32,35 @@ func init() {
 
 func rebuildFromCache() {
 	rootDir := viper.GetString("root_directory")
-	cacheDir := viper.GetString("cache_directory")
 	manifestFile := viper.GetString("manifest_file")
+	bucket := viper.GetString("s3.bucket")
 	
 	logger.Info("Starting rebuild from cache...")
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion(viper.GetString("s3.region")),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(
+			viper.GetString("s3.access_key_id"),
+			viper.GetString("s3.secret_access_key"),
+			"",
+		)),
+		config.WithEndpointResolverWithOptions(aws.EndpointResolverWithOptionsFunc(
+			func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+				return aws.Endpoint{
+					URL:           viper.GetString("s3.endpoint"),
+					SigningRegion: viper.GetString("s3.region"),
+					Source:        aws.EndpointSourceCustom,
+				}, nil
+			},
+		)),
+		)
+		if err != nil {
+			logger.Error("Failed to load AWS configuration", "error", err)
+			os.Exit(1)
+		}
+		s3Client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.UsePathStyle = true
+		})
 
 	manifestData, err := os.ReadFile(manifestFile)
 	if err != nil {
@@ -52,20 +81,23 @@ func rebuildFromCache() {
 	}
 
 	for destinationPath, hashString := range manifest {
-		logger.Debug("Rebuilding file", "path", destinationPath) // A good debug message
-		sourcePath := filepath.Join(cacheDir, hashString)
+		logger.Debug("Rebuilding file", "path", destinationPath)
+
+		result, err := s3Client.GetObject(context.TODO(), &s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(hashString),
+		})
+		if err != nil {
+			logger.Warn("Failed to retrieve object from S3, skipping", "bucket", bucket, "key", hashString, "error", err)
+			continue // Skip this file if it cannot be retrieved
+		}
+		defer result.Body.Close()
+
 		destinationDir := filepath.Dir(destinationPath)
 		if err := os.MkdirAll(destinationDir, 0755); err != nil {
 			logger.Error("Failed to create directory during rebuild", "path", destinationDir, "error", err)
-			os.Exit(1) // This is a fatal error
+			os.Exit(1) 
 		}
-
-		sourceFile, err := os.Open(sourcePath)
-		if err != nil {
-			logger.Error("Cache is corrupt. Could not open cache file.", "path", sourcePath, "error", err)
-			os.Exit(1) // Also fatal, the cache is broken
-		}
-		defer sourceFile.Close()
 
 		destFile, err := os.Create(destinationPath)
 		if err != nil {
@@ -74,8 +106,8 @@ func rebuildFromCache() {
 		}
 		defer destFile.Close()
 
-		if _, err := io.Copy(destFile, sourceFile); err != nil {
-			logger.Warn("Failed to copy content, file may be corrupt", "path", destinationPath, "error", err)
+		if _, err := io.Copy(destFile, result.Body); err != nil {
+			logger.Warn("Failed to write downloaded content to file", "path", destinationPath, "error", err)
 		}
 	}
 
